8537734dd10e715df0db3fa173c0f20d4c3e07e9
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2023-10-18 22:09:30 +0530

    Java polymorphism

---
+++ _20_polymorphism.java
@@ -0,0 +1,34 @@
+// polymorphism => many + behaviour
+// same object or same reference have different behaviour base on how you create instance of it
+
+// compile time polymorphism is METHOD OVERLOADING
+// run time polymorphism. is METHOD OVERRIDING
+
+public class _20_polymorphism 
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
+
+
+
+/*
+-- it is important concept of object oriented programming.
+-- one word in poly which means  many and words morphism means behaviour.
+-- the polymorphism means behaving one action differently in different situation in java. 
+
+-- two types of polymorphism
+
+-- compile time polymorphism and Run time polymorphism 
+
+-- when you compile the file and you will know what things get executed this behaviour is knows as compile time polymorphism. (OVERLOADING)
+
+-- when behaviour decide at run time this is known as run time polymorphism. (OVERRIDING)
+
+-- add(int, int) , add(int, int, int) whichb excutes decided at compile time 
+
+- class A extends B  -- A class  has add(int, int) and B has add(int,int ) out of this two we can decide which can execute at run time 
+                         then this is called run time polymorphism example.
+ */
