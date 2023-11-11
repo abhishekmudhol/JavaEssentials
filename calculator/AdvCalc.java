db21db9da3bea18ba7fa31c0f2f432cf8d5b4fce
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2023-11-11 21:47:04 +0530

    completed

---
+++ calculator/AdvCalc.java
@@ -0,0 +1,14 @@
+package calculator;
+public class AdvCalc extends calc {
+    
+             
+     public int multi(int n1,int n2) 
+     {
+           return n1 * n2;
+     }
+     public int div(int n1 , int n2)
+     {
+       return n1/n2;
+     }
+     
+}
---
+++ calculator/calc.java
@@ -0,0 +1,16 @@
+package calculator;
+
+
+ public class calc
+{
+         
+       public int add(int n1,int n2) //You should specify the data types of the parameters
+       {
+             return n1 + n2;
+       }
+
+       public int sub(int n1 , int n2)
+       {
+         return n1 - n2;
+       }
+}
